bc8d57623ba463caf77b5f4ef699bc7b18041a75
masoud9811 <masoud.shakarami89@gmail.com>
2023-03-23 12:17:35 +0430

    create get question

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,5 @@ app_name = 'home'
 
 urlpatterns  = [
     path('', views.HomeView.as_view(), name='home'),
+    path('question/', views.QuestionView.as_view())
 ]
--- home/views.py
+++ home/views.py
@@ -1,7 +1,26 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from .models import Question, Answer
+from .serializer import QuestionSerializer, AnswerSerializer
+from rest_framework import status
 
 
 class HomeView(APIView):
     def get(self, request):
         return Response({'name': 'masoud'})
+
+
+class QuestionView(APIView):
+    def get(self, request):
+        questions = Question.objects.all()
+        srz_data = QuestionSerializer(instance=questions, many=True)
+        return Response(srz_data.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        pass
+
+    def put(self, request, pk):
+        pass
+
+    def delete(self, request, pk):
+        pass
