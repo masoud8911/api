4bede57c06c7f74b28feadc68673c695f5225865
masoud9811 <masoud.shakarami89@gmail.com>
2023-03-29 11:20:51 +0430

    use ViewSet For user

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
 from . import views
 from rest_framework.authtoken import views as auth_view
+from rest_framework import routers
+
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('get_token_auth/', auth_view.obtain_auth_token)
 ]
+
+router = routers.SimpleRouter()
+router.register('user',views.UserViewSet)
+
+urlpatterns += router.urls
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets
+from .serializer import UserSerializer
+from django.contrib.auth.models import User
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+
+class UserViewSet(viewsets.ViewSet):
+    permission_classes = [IsAuthenticated, ]
+    queryset = User.objects.all()
+
+    def list(self, request):
+        srz_data = UserSerializer(instance=self.queryset, many=True)
+        return Response(srz_data.data)
+
+    def retrieve(self, request, pk=None):
+        user = get_object_or_404(self.queryset, pk=pk)
+        srz_data = UserSerializer(instance=user)
+        return Response(data=srz_data.data)
+
+    def partial_update(self, request, pk=None):
+        user = get_object_or_404(self.queryset, pk=pk)
+        if user != request.user:
+            return Response({'message': 'You are not the owner'})
+        srz_data = UserSerializer(instance=user, data=request.POST, partial=True)
+        if srz_data.is_valid():
+            srz_data.save()
+            return Response(data=srz_data.data)
+        return Response(data=srz_data.errors)
+
+    def destroy(self, request, pk=None):
+        user = get_object_or_404(self.queryset, pk=pk)
+        user.is_active = False
+        user.save()
+        return Response({'massage': 'user  deactivated'})
