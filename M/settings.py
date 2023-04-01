03fd082c8681a68840b4dbcf46e0f2c71f984cff
masoud9811 <masoud.shakarami89@gmail.com>
2023-04-01 10:35:26 +0430

    use JWT for authentication

--- M/settings.py
+++ M/settings.py
@@ -119,7 +119,8 @@ USE_TZ = True
 
 REST_FRAMEWORK = {
    'DEFAULT_AUTHENTICATION_CLASSES': (
-       'rest_framework.authentication.TokenAuthentication',
+       # 'rest_framework.authentication.TokenAuthentication',
+       'rest_framework_simplejwt.authentication.JWTAuthentication',
    ),
    'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAdminUser'
--- accounts/urls.py
+++ accounts/urls.py
@@ -2,15 +2,21 @@ from django.urls import path
 from . import views
 from rest_framework.authtoken import views as auth_view
 from rest_framework import routers
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
 app_name = 'accounts'
 
 urlpatterns = [
-    path('get_token_auth/', auth_view.obtain_auth_token)
+    path('get_token_auth/', auth_view.obtain_auth_token),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
 router = routers.SimpleRouter()
 router.register('user',views.UserViewSet)
 
 urlpatterns += router.urls
+
+# "refresh": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoicmVmcmVzaCIsImV4cCI6MTY4MDQxNTE5MiwiaWF0IjoxNjgwMzI4NzkyLCJqdGkiOiI0MWUyODAyNGMwODg0NWJlOWM4Y2ZhN2Q3NWY2NWM5NiIsInVzZXJfaWQiOjF9.WNhcSD6C8W25kV62RxmOYu97eR6NH-5kWhaVXeLSfMI",
+# "access": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjgwMzI5MDkyLCJpYXQiOjE2ODAzMjg3OTIsImp0aSI6IjY1YzZiOGYwY2M4ZDQwY2I5MTViMjZmYzdiODFiODNkIiwidXNlcl9pZCI6MX0.1zZBEZM1Yxkdox9OhbfQuyhsWO-B3bD70-W-hGRTrCA"
